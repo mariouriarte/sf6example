da44fa1ad5fe49e50677c701849028bbe978b7d3
Mario Uriarte <mario.uriarte@gmail.com>
2024-10-31 10:44:49 -0400

    Adición de links

--- src/Controller/MultiRoutesController.php
+++ src/Controller/MultiRoutesController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 class MultiRoutesController extends AbstractController
 {
     #[Route('/browse1', name: 'browser1')]
